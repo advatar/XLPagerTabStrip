b02b05f585e43838da1542581a137e32eb5fb98e
Johan Sellström <johan.sellstrom@gmail.com>
2021-09-04 13:15:24 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,17 @@
 
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "XLPagerTabStrip",
+    platforms: [.iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "XLPagerTabStrip", targets: ["XLPagerTabStrip"])
+    ],
+    targets: [
+        .target(
+            name: "XLPagerTabStrip",
+            path: "Sources"
+        )
+    ]
+)
